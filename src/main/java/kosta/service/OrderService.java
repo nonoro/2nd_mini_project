2612ac9079bf0fe132f36299ee47e93277b33b55
minjeong <minjeongg24@gmail.com>
2022-10-24 10:10:32 +0900

    feature: 장바구니 기능 일부 구현 & 상품 주문 기능 일부 수정

--- src/main/java/kosta/service/OrderService.java
+++ src/main/java/kosta/service/OrderService.java
@@ -4,13 +4,16 @@ import java.sql.SQLException;
 import java.util.List;
 
 import kosta.dto.OrderDTO;
-import kosta.dto.OrderLineDTO;
-import kosta.dto.PointDTO;
-import kosta.dto.UserDTO;
 
 public interface OrderService {
+	/**
+	 * 주문하기
+	 */
 	int orderInsert(OrderDTO order) throws SQLException;
 
+	/**
+	 * 주문 취소
+	 * */
 	int orderCancel(OrderDTO order) throws SQLException;
 	
 	/**
